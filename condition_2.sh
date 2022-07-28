6cbd0f462fd701e4d5805b579e72e8e6ce9b66d3
root <alam.mdnazmul@outlook.com>
2022-07-28 07:40:20 -0400

    27-Jul-22

---
+++ condition_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MY_SHELL="bash"
+
+if [ ${MY_SHELL} == "bash" ]
+then
+	echo "You are in bash shell"
+else
+	echo "You aren't in bash shell"
+fi
---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MY_SHELL="bash"
+
+echo "I like the ${MY_SHELL} shell"
+
+echo "I am ${MY_SHELL}ing on my PC"
