a39fc512096e1015b71c1a44df31d3ba64e80cec
root <alam.mdnazmul@outlook.com>
2022-07-28 19:13:14 -0400

    29-Jul-22

---
+++ functions/positional_parameter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+function hello() {
+	echo "Hi $1"
+}
+
+hello Nazmul
---
+++ functions/positional_parameter_loop_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function hello() {
+	for NAME in $@
+	do
+		echo "I am ${NAME}"
+	done
+}
+
+hello Nazmul
